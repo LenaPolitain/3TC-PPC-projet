4aa52e7cb6a8d51cd39a7be62df0db8924521d62
EoliaBagnoli <eolia.bagnoli@insa-lyon.fr>
2023-01-25 10:12:03 +0100

    2nd commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-hello Léna
+from multiprocessing import Process, Array
+import threading
+import os 
+import time 
+import socket 
+import sys
+from queue import Queue
+
+# variables globales pour la shared memory : weather
+# dictionnaire de couples de trucs méteo
+meteo_size = 12
+shared_meteo = Array('L', meteo_size) 
+
+
+
+
+def home() : 
+    print("home")
+
+def market() :
+    print("market")
+
+def weather(meteo) : 
+    print("weather")
+
+
+
+if __name__ == "__main___" : 
+    home = Process(target = home, args=())
+    market = Process(target= market, args=())
+    weather = Process(target = weather, args = (shared_meteo,))
+    home.start() 
+    market.start()
+    weather.start()
+    home.join() 
+    market.join()
+    weather.join()
