1e2f85cfa8ca438d1a2520bbbab9adc1c2486258
EoliaBagnoli <eolia.bagnoli@insa-lyon.fr>
2023-01-29 14:49:52 +0100

    temperature modifications

--- weather.py
+++ weather.py
@@ -2,8 +2,16 @@ import time
 import random 
 from multiprocessing import Value
 
+
+# la temperature croit ou décroit de facon random toutes les secondes
+
 mod_temp = 0.001
 
 def weather(current_temp) : 
-    time.sleep(1)
-    current_temp.value = random.randint(10, 35)
+    while True : 
+        aleatoire = random.choice(True, False)
+        if aleatoire == True : 
+            current_temp.value = current_temp.value + 1
+        else : 
+            current_temp.value = current_temp.value - 1
+        time.sleep(1)
